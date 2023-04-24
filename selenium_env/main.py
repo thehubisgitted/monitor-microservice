bcabecf28ae33345260ea604530cb8bfe423fb1c
Kenneth Pinzon <kennethpinzonrojas@gmail.com>
2023-04-24 18:01:58 -0500

    add .py within env to git

---
+++ selenium_env/main.py
@@ -0,0 +1,10 @@
+#INPUTS 
+#List of websites to monitor
+#Proxies for monitoring and purchasing
+#Whatever it needs to communicate with the discord bot
+#User Information if it's specific to a user requesting to purchase an item.
+
+#OUTPUTS
+#message to the discord bot for pinging
+#recording user purchases
+#recording diagnostics for if it crashes / gets blocked
